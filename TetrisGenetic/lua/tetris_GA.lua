68d4f816a2fca9c396e6c4f6307a2b17b421c8e7
Alan Chen <avchen4@gmail.com>
2020-06-11 22:51:41 -0400

    created get_population_fitness function

--- TetrisGenetic/lua/tetris_GA.lua
+++ TetrisGenetic/lua/tetris_GA.lua
@@ -1,3 +1,5 @@
+require('lua_helper')
+
 -- generates initial population
 function init_population(size, move_limit)
     population = {}
@@ -8,4 +10,12 @@ function init_population(size, move_limit)
 end
 
 function get_population_fitness(population, move_limit, generation)
+    local temp_population = deepcopy(population)
+    for i = 1, #temp_population do
+        -- get fitness
+        if (not temp_population[i].fitness) then
+           temp_population[i].fitness = play_game(temp_population[i], move_limit)
+       end
+   end
+   return temp_population
 end
