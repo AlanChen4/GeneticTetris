2f6f1047889fa0e35af2d109cc8c3aa3f37c3009
Alan Chen <avchen4@gmail.com>
2020-11-28 12:05:32 -0500

    Fix indenting

--- TetrisGenetic/lua/game_helper.lua
+++ TetrisGenetic/lua/game_helper.lua
@@ -10,82 +10,82 @@ end
 
 
 function reset_buttons()
-  local buttons = {A=false, up=false, left=false, B=false, select=false, right=false, down=false, start=false}
-  joypad.set(1, buttons)
-  emu.frameadvance()
+    local buttons = {A=false, up=false, left=false, B=false, select=false, right=false, down=false, start=false}
+    joypad.set(1, buttons)
+    emu.frameadvance()
 end
 
 
 function move_right()
-  local buttons = joypad.get(1)
-  buttons['right'] = true
-  joypad.set(1, buttons)
-  emu.frameadvance()
-  reset_buttons()
+    local buttons = joypad.get(1)
+    buttons['right'] = true
+    joypad.set(1, buttons)
+    emu.frameadvance()
+    reset_buttons()
 end
 
 
 function move_left()
-  local buttons = joypad.get(1)
-  buttons['left'] = true
-  joypad.set(1, buttons)
-  emu.frameadvance()
-  reset_buttons()
+    local buttons = joypad.get(1)
+    buttons['left'] = true
+    joypad.set(1, buttons)
+    emu.frameadvance()
+    reset_buttons()
 end
 
 
 function move_down()
-  local buttons = joypad.get(1)
-  buttons['down'] = true
-  joypad.set(1, buttons)
-  emu.frameadvance()
-  reset_buttons()
+    local buttons = joypad.get(1)
+    buttons['down'] = true
+    joypad.set(1, buttons)
+    emu.frameadvance()
+    reset_buttons()
 end
 
 
 function move_up()
-  local buttons = joypad.get(1)
-  buttons['up'] = true
-  joypad.set(1, buttons)
-  emu.frameadvance()
-  reset_buttons()
+    local buttons = joypad.get(1)
+    buttons['up'] = true
+    joypad.set(1, buttons)
+    emu.frameadvance()
+    reset_buttons()
 end
 
 
 function press_start()
-  local buttons = joypad.get(1)
-  buttons['start'] = true
-  joypad.set(1, buttons)
-  emu.frameadvance()
-  reset_buttons()
+    local buttons = joypad.get(1)
+    buttons['start'] = true
+    joypad.set(1, buttons)
+    emu.frameadvance()
+    reset_buttons()
 end
 
 
 function rotate()
-  local buttons = joypad.get(1)
-  buttons['A'] = true
-  joypad.set(1, buttons)
-  emu.frameadvance()
-  reset_buttons()
+    local buttons = joypad.get(1)
+    buttons['A'] = true
+    joypad.set(1, buttons)
+    emu.frameadvance()
+    reset_buttons()
 end
 
 -- simulates sleep by skipping five frames
 function tetris_sleep()
-  emu.frameadvance()
-  emu.frameadvance()
-  emu.frameadvance()
-  emu.frameadvance()
-  emu.frameadvance()
+    emu.frameadvance()
+    emu.frameadvance()
+    emu.frameadvance()
+    emu.frameadvance()
+    emu.frameadvance()
 end
 
 
 function dec_to_hex(dec_val)
-  local total = 0
-  local base = 1
-  while (dec_val > 0) do
+    local total = 0
+    local base = 1
+    while (dec_val > 0) do
     total = total + (dec_val % 16) * base
     dec_val = math.floor(dec_val / 16)
     base = base * 10
-  end
-  return total
+    end
+    return total
 end
